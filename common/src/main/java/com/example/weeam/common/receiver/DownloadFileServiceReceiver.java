4ee7997f26f8df62892af3a602a1160795071997
Weeam Awad <weeam93@gmail.com>
2018-08-12 17:35:56 -0700

    Initial commit

---
+++ common/src/main/java/com/example/weeam/common/receiver/DownloadFileServiceReceiver.java
@@ -0,0 +1,17 @@
+package com.example.weeam.common.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Weeam Awad on 5/6/2018.
+ */
+
+public class DownloadFileServiceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, DownloadFileServiceReceiver.class);
+        context.stopService(service);
+    }
+}
